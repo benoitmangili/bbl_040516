5da1ac02398597c97d8915fc70f8af29fba1b0f4
Benoit Mangili <benoit.mangili@tessella.com>
2016-05-04 11:51:22 +0100

    removing the hot reload

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ var WebpackDevServer = require('webpack-dev-server');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
-  hot: true,
   historyApiFallback: true,
   stats:{ colors: true}
 
